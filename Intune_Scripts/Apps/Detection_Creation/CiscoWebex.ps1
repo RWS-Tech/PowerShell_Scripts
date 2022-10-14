8dc957c4e566a75587a2b9a2a361a1ac9485cdaa
Dralzin <61769733+stadelmanr@users.noreply.github.com>
2022-10-14 10:42:34 -0500

    Added CiscoWebex.ps1
    Script that Creates the Detection Script for Cisco Webex

---
+++ Intune_Scripts/Apps/Detection_Creation/CiscoWebex.ps1
@@ -0,0 +1,25 @@
+## Check for Cisco Webex (Registry Detection Method)
+$RegKey1 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$RegKey2 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+$AppName = 'Webex'
+$Uninstall1 = 'MsiExec.exe /I'
+$Uninstall2 = 'MsiExec.exe /X'
+
+$Webex = Get-ChildItem -Path $RegKey1,$RegKey2 | Get-ItemProperty | Where-Object {$_.DisplayName -match $AppName } | Select-Object -Property DisplayName, DisplayVersion, UninstallString
+$Webex.DisplayVersion
+$GUID = $Webex.UninstallString -replace $Uninstall1, '' -replace $Uninstall2, ''
+
+## Create Text File with Cisco Webex Registry Detection Method
+$FilePath = "C:\Temp\Cisco_Webex_Detection_Method.ps1"
+$RegGUID = "\$($GUID)"
+$RegPath1 = $RegKey1 + $RegGUID
+$RegPath2 = $RegKey2 + $RegGUID
+
+New-Item -Path "$FilePath" -Force
+Set-Content -Path "$FilePath" -Value "`$AppVersion = '$($Webex.DisplayVersion)'"
+Add-Content -Path "$FilePath" -Value "`$RegPath1 = '$($RegPath1)'"
+Add-Content -Path "$FilePath" -Value "`$RegPath2 = '$($RegPath2)'"
+Add-Content -Path "$FilePath" -Value "If([Version](Get-ItemPropertyValue -Path `$RegPath1,`$RegPath2 -Name DisplayVersion -ea SilentlyContinue) -ge `$AppVersion) {"
+Add-Content -Path "$FilePath" -Value "Write-Host `"Installed`""
+Add-Content -Path "$FilePath" -Value "}"
+Invoke-Item $FilePath
