23dda8c8ef1109cb8df78097a42a7cfef45662d0
Dralzin <61769733+stadelmanr@users.noreply.github.com>
2022-10-14 13:53:25 -0500

    Add files via upload

---
+++ Intune_Scripts/Apps/Detection/Visual_Studio_Code_Detection_Method.ps1
@@ -0,0 +1,6 @@
+$AppVersion = 'x.xx.x'
+$64BitPath = 'C:\Program Files\Microsoft VS Code\Code.exe'
+$32BitPath = 'C:\Program Files (x86)\Microsoft VS Code\Code.exe'
+If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge $AppVersion){
+Write-Host "Installed"
+}
