7419aeee00de58f83a38adb96a60b05b113c6c11
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-14 12:58:35 -0500

    Create CiscoWebexMeetings.ps1

---
+++ Intune_Scripts/Apps/Detection_Creation/CiscoWebexMeetings.ps1
@@ -0,0 +1,32 @@
+#Registry location containing the key
+$RegKey1 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$RegKey2 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+
+#Application Display Name
+$AppName = 'Webex Meetings'
+
+#Uninstalll Commands
+$Uninstall1 = 'MsiExec.exe /I'
+$Uninstall2 = 'MsiExec.exe /X'
+
+$App = Get-ChildItem -Path $RegKey1,$RegKey2 | Get-ItemProperty | Where-Object {$_.DisplayName -match $AppName } | Select-Object -Property DisplayName, DisplayVersion, UninstallString
+$App.DisplayVersion
+$GUID = $App.UninstallString -replace $Uninstall1, '' -replace $Uninstall2, ''
+
+## Create Script File with Application Registry Detection Method
+$FileAppName = "Cisco_Webex_Meetings"
+$FileName = $FileAppName + "_Detection_Method.ps1"
+$FileRoot = "C:\Temp\"
+$FilePath = $FileRoot + $FileName
+$RegGUID = "\$($GUID)"
+$RegPath1 = $RegKey1 + $RegGUID
+$RegPath2 = $RegKey2 + $RegGUID
+
+New-Item -Path "$FilePath" -Force
+Set-Content -Path "$FilePath" -Value "`$AppVersion = '$($App.DisplayVersion)'"
+Add-Content -Path "$FilePath" -Value "`$RegPath1 = '$($RegPath1)'"
+Add-Content -Path "$FilePath" -Value "`$RegPath2 = '$($RegPath2)'"
+Add-Content -Path "$FilePath" -Value "If([Version](Get-ItemPropertyValue -Path `$RegPath1,`$RegPath2 -Name DisplayVersion -ea SilentlyContinue) -ge `$AppVersion) {"
+Add-Content -Path "$FilePath" -Value "Write-Host `"Installed`""
+Add-Content -Path "$FilePath" -Value "}"
+Invoke-Item $FilePath
