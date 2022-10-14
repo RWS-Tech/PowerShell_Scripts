19724dee2fd769aaa41e9383cf07b9df17872f25
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-14 13:51:11 -0500

    Create VisualStudioCode.ps1

---
+++ Intune_Scripts/Apps/Detection_Creation/VisualStudioCode.ps1
@@ -0,0 +1,31 @@
+# 64-Bit File Path, add the rest of the file path
+$64BitPath = "$Env:ProgramFiles\Microsoft VS Code\Code.exe"
+
+# 32-Bit File, add the rest of the file path
+$32BitPath = "${Env:ProgramFiles(x86)}\Microsoft VS Code\Code.exe"
+
+# Check for Application (File Detection Method)
+$AppExe = (Get-ChildItem -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue)
+$AppExe.FullName
+$AppPath = $($AppExe.FullName).Replace("$Env:ProgramFiles\","").Replace("${Env:ProgramFiles(x86)}\","")
+$FileVersion = (Get-Item -Path "$($AppExe.FullName)" -ErrorAction SilentlyContinue).VersionInfo.FileVersion
+
+# Create the script with the Application Detection Method
+# Contains the name of the Application
+$AppName = "Visual_Studio_Code"
+$FileName = $AppName + "_Detection_Method.ps1"
+
+#Contains the location where the script will be saved
+$SRoot = "C:\Temp\" 
+
+$FilePath = $SRoot + $FileName
+
+
+New-Item -Path "$FilePath" -Force
+Set-Content -Path "$FilePath" -Value "`$AppVersion = '$FileVersion'"
+Add-Content -Path "$FilePath" -Value "`$64BitPath = '$64BitPath'"
+Add-Content -Path "$FilePath" -Value "`$32BitPath = '$32BitPath'"
+Add-Content -Path "$FilePath" -Value "If([String](Get-Item -Path `$64BitPath,`$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge `$AppVersion){"
+Add-Content -Path "$FilePath" -Value "Write-Host `"Installed`""
+Add-Content -Path "$FilePath" -Value "}"
+Invoke-Item $FilePath
