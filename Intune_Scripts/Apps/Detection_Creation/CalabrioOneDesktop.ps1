22561b686e51d0c0004e637ebd05bce87b1fce4f
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 11:30:42 -0500

    Update CalabrioOneDesktop.ps1

--- Intune_Scripts/Apps/Detection_Creation/CalabrioOneDesktop.ps1
+++ Intune_Scripts/Apps/Detection_Creation/CalabrioOneDesktop.ps1
@@ -1,10 +1,13 @@
+# 64-Bit File Path, add the rest of the file path
+$64BitPath = "$Env:ProgramFiles\Calabrio One\Desktop\active\bin\DCC.exe"
+
 # 32-Bit File, add the rest of the file path
 $32BitPath = "${Env:ProgramFiles(x86)}\Calabrio One\Desktop\active\bin\DCC.exe"
 
 # Check for Application (File Detection Method)
-$AppExe = (Get-ChildItem -Path $32BitPath -ErrorAction SilentlyContinue)
+$AppExe = (Get-ChildItem -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue)
 $AppExe.FullName
-$AppPath = $($AppExe.FullName).Replace("${Env:ProgramFiles(x86)}\","")
+$AppPath = $($AppExe.FullName).Replace("$Env:ProgramFiles\","").Replace("${Env:ProgramFiles(x86)}\","")
 $FileVersion = (Get-Item -Path "$($AppExe.FullName)" -ErrorAction SilentlyContinue).VersionInfo.FileVersion
 
 # Create the script with the Application Detection Method
@@ -17,10 +20,12 @@ $SRoot = "C:\Temp\"
 
 $FilePath = $SRoot + $FileName
 
+
 New-Item -Path "$FilePath" -Force
 Set-Content -Path "$FilePath" -Value "`$AppVersion = '$FileVersion'"
+Add-Content -Path "$FilePath" -Value "`$64BitPath = '$64BitPath'"
 Add-Content -Path "$FilePath" -Value "`$32BitPath = '$32BitPath'"
-Add-Content -Path "$FilePath" -Value "If([String](Get-Item -Path `$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge `$AppVersion){"
+Add-Content -Path "$FilePath" -Value "If([String](Get-Item -Path `$64BitPath,`$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge `$AppVersion){"
 Add-Content -Path "$FilePath" -Value "Write-Host `"Installed`""
 Add-Content -Path "$FilePath" -Value "}"
 Invoke-Item $FilePath
