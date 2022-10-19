6260f5750a994a0b47388e45fcf413da02b0a7be
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 12:30:22 -0500

    Update Dell_Command_Update_Detection_Method.ps1

--- Intune_Scripts/Apps/Detection/Dell_Command_Update_Detection_Method.ps1
+++ Intune_Scripts/Apps/Detection/Dell_Command_Update_Detection_Method.ps1
@@ -1,6 +1,3 @@
-# This contains the version to check for.
-$AppVersion = "x.x.x.x"
-
 # This contains the path to the 64-Bit installation.
 $64BitPath = "$Env:ProgramFiles\Dell\CommandUpdate\dcu-cli.exe"
 
@@ -9,6 +6,6 @@ $32BitPath = "${Env:ProgramFiles(x86)}\Dell\CommandUpdate\dcu-cli.exe"
 
 # The following code checks to make sure the application is installed
 # at the assigned version.
-If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge $AppVersion){
+If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion){
  Write-Host "Installed"
 }
