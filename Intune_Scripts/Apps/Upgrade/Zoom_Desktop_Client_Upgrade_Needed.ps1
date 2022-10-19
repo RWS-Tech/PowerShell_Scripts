3244e5aa54e12f9968831f1b2bf1f6119a31acb8
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 11:06:47 -0500

    Update Zoom_Desktop_Client_Upgrade_Needed.ps1

--- Intune_Scripts/Apps/Upgrade/Zoom_Desktop_Client_Upgrade_Needed.ps1
+++ Intune_Scripts/Apps/Upgrade/Zoom_Desktop_Client_Upgrade_Needed.ps1
@@ -1,6 +1,6 @@
 $64BitPath = 'C:\Program Files\Zoom\bin\Zoom.exe'
 $32BitPath = 'C:\Program Files (x86)\Zoom\bin\Zoom.exe'
-If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge $AppVersion){
+If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion){
 Write-Host "Update"
 }
 else {
