9ea758da0ded1985ee07e01fd0c6b60abb14a9eb
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 08:47:40 -0500

    Updated Scripts

--- Intune_Scripts/Apps/Upgrade/Citrix_Workspace_Upgrade_Needed.ps1
+++ Intune_Scripts/Apps/Upgrade/Citrix_Workspace_Upgrade_Needed.ps1
@@ -1,6 +1,9 @@
 $AppVersion = 'xx.x.x.xx'
 $64BitPath = 'C:\Program Files\Citrix\Citrix WorkSpace*\TrolleyExpress.exe'
 $32BitPath = 'C:\Program Files (x86)\Citrix\Citrix WorkSpace*\TrolleyExpress.exe'
-If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -lt $AppVersion){
-Write-Host "Installed"
+If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge $AppVersion){
+Write-Host "Up-to-date"
+}
+Else {
+Write-Host "Update"
 }
