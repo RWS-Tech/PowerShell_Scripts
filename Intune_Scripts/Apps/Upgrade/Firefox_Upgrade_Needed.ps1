6834732404e8197556021ff4cd1b6c7300b283c4
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 10:55:19 -0500

    Updated Scripts

--- Intune_Scripts/Apps/Upgrade/Firefox_Upgrade_Needed.ps1
+++ Intune_Scripts/Apps/Upgrade/Firefox_Upgrade_Needed.ps1
@@ -1,4 +1,3 @@
-$AppVersion = 'xxx.x.x'
 $64BitPath = 'C:\Program Files\Mozilla Firefox\firefox.exe'
 $32BitPath = 'C:\Program Files (x86)\Mozilla Firefox\firefox.exe'
 If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion){
--- Intune_Scripts/Apps/Upgrade/Google_Chrome_Upgrade_Needed.ps1
+++ Intune_Scripts/Apps/Upgrade/Google_Chrome_Upgrade_Needed.ps1
@@ -1,4 +1,3 @@
-$AppVersion = 'xxx.x.xxxx.xxx'
 $64BitPath = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
 $32BitPath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 If([String](Get-Item -Path $64BitPath,$32BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion){
