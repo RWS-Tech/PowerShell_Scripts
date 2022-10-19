d9d5ca872c663cd08c44aaa573bfa2dc4be2049c
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 12:26:43 -0700

    Added TimeZone Detection

---
+++ Intune_Scripts/Apps/Detection/Timezone_CST_Detection_Method.ps1
@@ -0,0 +1,7 @@
+$TZone = "Central Standard Time"
+$RegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation"
+$CTZone = [String](Get-ItemPropertyValue -Path $RegPath -Name TimeZoneKeyName -ea SilentlyContinue)
+
+If($CTZone -eq $TZone) {
+Write-Host "Installed"
+}
---
+++ Intune_Scripts/Apps/Detection/Timezone_MST_Detection_Method.ps1
@@ -0,0 +1,7 @@
+$TZone = "Mountain Standard Time"
+$RegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation"
+$CTZone = [String](Get-ItemPropertyValue -Path $RegPath -Name TimeZoneKeyName -ea SilentlyContinue)
+
+If($CTZone -eq $TZone) {
+Write-Host "Installed"
+}
