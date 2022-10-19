3c68c623139ef97aa217b4cbf7183e277aa3201f
stadelmanr <61769733+stadelmanr@users.noreply.github.com>
2022-10-19 12:12:51 -0500

    Added Dell Command Update

---
+++ Intune_Scripts/Apps/Upgrade/Dell_Command_Update_UWP_Upgrade_Needed.ps1
@@ -0,0 +1,11 @@
+# This contains the path to the 64-Bit installation.
+$64BitPath = "$Env:ProgramFiles\Dell\CommandUpdate\dcu-cli.exe"
+
+# The following code checks to make sure the application is installed
+# at the assigned version.
+If([String](Get-Item -Path $64BitPath -ErrorAction SilentlyContinue).VersionInfo.FileVersion){
+Write-Host "Update"
+}
+Else {
+Write-Host "Not Installed"
+}
