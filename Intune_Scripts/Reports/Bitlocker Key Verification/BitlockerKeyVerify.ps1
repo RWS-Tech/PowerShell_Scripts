8a58c405ff58af990a1794a57c2cc26c70c5b1aa
Dralzin <61769733+stadelmanr@users.noreply.github.com>
2023-05-31 12:09:26 -0500

    Create BitlockerKeyVerify.ps1

---
+++ Intune_Scripts/Reports/Bitlocker Key Verification/BitlockerKeyVerify.ps1
@@ -0,0 +1,13 @@
+Function CheckBitlockerKey {
+    cls
+    If ((Get-BitLockerVolume -MountPoint C).KeyProtector) {
+        $KeyID = ((Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object -Property KeyProtectorType -EQ "RecoveryPassword").KeyProtectorId
+        $RecPass = ((Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object -Property KeyProtectorType -EQ "RecoveryPassword").RecoveryPassword
+    
+        Write-Output "Key ID: $($KeyID)    Recovery Password: $($RecPass)"
+    } Else {
+        Write-Output "No Bitlocker Encryption Detected"
+    }    
+    Exit 0
+}
+CheckBitlockerKey
