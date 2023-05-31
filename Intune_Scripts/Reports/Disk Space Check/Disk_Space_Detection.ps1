974e837ca1df82519bf067f617850d7b737ead66
Dralzin <61769733+stadelmanr@users.noreply.github.com>
2023-05-31 11:15:05 -0500

    Disk Space Check Added

---
+++ Intune_Scripts/Reports/Disk Space Check/Disk_Space_Detection.ps1
@@ -0,0 +1,66 @@
+function DiskSpaceChk {
+    $DskChkFile = "C:\Temp\dskchk.txt"
+    $Percent_Alert = "10"
+    $Percent_Warn = "25"
+    $Win32_LogicalDisk = Get-CimInstance Win32_LogicalDisk | Where {$_.DeviceID -eq "C:"}
+    $Disk_Full_Size = $Win32_LogicalDisk.Size
+    $Disk_Free_Space = $Win32_LogicalDisk.FreeSpace
+    $GB_Free = [Math]::Round(((($Disk_Free_Space / 1024) / 1024) / 1024),1)
+    $Total_Size_NoFormat = [Math]::Round(($Disk_Full_Size))
+    $GB_Total = [Math]::Round(((($Total_Size_NoFormat / 1024) / 1024) / 1024),1)
+    [int]$Free_Space_Percent = '{0:N0}' -f (($Disk_Free_Space / $Total_Size_NoFormat * 100),1)
+    $DskMsgFile = "C:\Temp\dskmsg.txt"
+    $DskWrnFile = "C:\Temp\dskwrn.txt"
+    $LowDskSpc = "Low Disk Space"
+    $OkDskSpc = "Disk Space is nearing low level"
+    $GoodDskSpc = "Disk Space is good"
+
+    If( -not (Test-Path -Path $DskChkFile -PathType Leaf)) {
+        If($Free_Space_Percent -le $Percent_Alert) {
+            Write-Output "% Free: $($Free_Space_Percent)%,  GB Free: $($GB_Free) GB,  Total GB: $($GB_Total) GB"
+            $DiskMsg = $LowDskSpc
+            $DiskWrn = "1"
+            $DiskMsg | Out-File $DskMsgFile
+            $DiskWrn | Out-File $DskWrnFile
+            Exit 1
+        } Elseif (($Free_Space_Percent -gt $Percent_Alert) -and ($Free_Space_Percent -le $Percent_Warn)) {
+            Write-Output "% Free: $($Free_Space_Percent)%,  GB Free: $($GB_Free) GB,  Total GB: $($GB_Total) GB"
+            $DiskMsg = $OkDskSpc
+            $DiskWrn = "2"
+            $DiskMsg | Out-File $DskMsgFile
+            $DiskWrn | Out-File $DskWrnFile
+            Exit 1
+        } Elseif ($Free_Space_Percent -gt $Percent_Warn) {
+            Write-Output "% Free: $($Free_Space_Percent)%,  GB Free: $($GB_Free) GB,  Total GB: $($GB_Total) GB"
+            $DiskMsg = $GoodDskSpc
+            $DiskWrn = "2"
+            $DiskMsg | Out-File $DskMsgFile
+            $DiskWrn | Out-File $DskWrnFile
+            Exit 1
+        }
+    } Else {
+        $DiskMsg = Get-Content -Path $DskMsgFile
+        $DiskWrn = Get-Content -Path $DskWrnFile
+
+        If($DiskWrn -eq "1") {
+            Write-Warning $DiskMsg
+        } Else {
+            Write-Output $DiskMsg
+        }
+
+        If(Test-Path -Path $DskChkFile -PathType Leaf) {
+            Remove-Item -Path $DskChkFile
+        }
+
+        If (Test-Path -Path $DskMsgFile -PathType Leaf) {
+            Remove-Item -Path $DskMsgFile
+        }
+
+        If(Test-Path -Path $DskWrnFile -PathType Leaf) {
+            Remove-Item -Path $DskWrnFile
+        }
+
+        Exit 0
+    }
+}
+DiskSpaceChk
---
+++ Intune_Scripts/Reports/Disk Space Check/Disk_Space_Remediation.ps1
@@ -0,0 +1,15 @@
+Function DiskSpaceVerify {
+    $DskMsgFile = "C:\Temp\dskmsg.txt"
+    $DskWrnFile = "C:\Temp\dskwrn.txt"
+    $DskChkFile = "C:\Temp\dskchk.txt"
+    If((Test-Path -Path $DskMsgFile -PathType Leaf) -and (Test-Path -Path $DskWrnFile -PathType Leaf)) {
+        $DiskChk = $true
+        $DiskChk | Out-File $DskChkFile
+        Exit 0
+    } Else {
+        $DiskChk = $false
+        $DiskChk | Out-File $DskChkFile
+        Exit 0
+    }
+}
+DiskSpaceVerify
