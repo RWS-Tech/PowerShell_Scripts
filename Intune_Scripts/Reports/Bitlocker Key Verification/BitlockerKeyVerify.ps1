53463b8680cb4caeefff45ec7f12e2e9cfcda983
Dralzin <61769733+stadelmanr@users.noreply.github.com>
2023-06-01 09:26:52 -0500

    Update BitlockerKeyVerify.ps1

--- Intune_Scripts/Reports/Bitlocker Key Verification/BitlockerKeyVerify.ps1
+++ Intune_Scripts/Reports/Bitlocker Key Verification/BitlockerKeyVerify.ps1
@@ -1,6 +1,6 @@
 Function CheckBitlockerKey {
     cls
-    If ((Get-BitLockerVolume -MountPoint C).KeyProtector) {
+    If ((Get-BitLockerVolume -MountPoint C -ErrorAction SilentlyContinue).KeyProtector) {
         $KeyID = ((Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object -Property KeyProtectorType -EQ "RecoveryPassword").KeyProtectorId
         $RecPass = ((Get-BitLockerVolume -MountPoint C).KeyProtector | Where-Object -Property KeyProtectorType -EQ "RecoveryPassword").RecoveryPassword
     
