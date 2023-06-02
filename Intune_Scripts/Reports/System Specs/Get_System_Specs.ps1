8f919aa37d4656c32bfd68e14e3dad8b2ae608c9
Dralzin <61769733+stadelmanr@users.noreply.github.com>
2023-06-02 11:11:22 -0500

    Update Get_System_Specs.ps1

--- Intune_Scripts/Reports/System Specs/Get_System_Specs.ps1
+++ Intune_Scripts/Reports/System Specs/Get_System_Specs.ps1
@@ -17,6 +17,32 @@ Function GetSysInfo {
     $HDFree = [Math]::Round((((($HDInfo.FreeSpace) / 1024) / 1024) / 1024),2)
     $NetName = $NetConInfo.Name 
     $NetType = $NetConInfo.NetConnectionID
+    $VidAdapters = Get-WmiObject Win32_VideoController
+
+    Foreach ($VidAdapter in $VidAdapters) {
+        $VidMaker = $VidAdapter.AdapterCompatibility
+        $VidName = $VidAdapter.Name
+        $VidType = $VidAdapter.AdapterDACType
+        $VidRAM = [Math]::Round((((($VidAdapter.AdapterRAM) / 1024) / 1024) / 1024),0)
+        $VidWidth = $VidAdapter.CurrentHorizontalResolution
+        $VidHeight = $VidAdapter.CurrentVerticalResolution
+        $VidColorDepth = $VidAdapter.CurrentBitsPerPixel
+        $VidRefreshRate = $VidAdapter.CurrentRefreshRate
+        $VidProc = $VidAdapter.VideoProcessor
+
+        $VNOut = "Name: $($VidName) `n"
+        $VMOut = "Manufacturer: $($VidMaker) `n"
+        $VTOut = "Type: $($VidType) `n"
+        $VROut = "Memory: $($VidRAM) GB `n"
+        $VResOut = "Current Resolution: $($VidWidth) x $($VidHeight) `n"
+        $VCDOut = "Current Color Depth: $($VidColorDepth) Bit `n"
+        $VRROut = "Current Refresh Rate: $($VidRefreshRate) Hz `n"
+        $VPOut = "Video Processor Name: $($VidProc) `n`n"
+
+        $VInfoOut = "$($VNOut)$($VMOut)$($VTOut)$($VROut)$($VResOut)$($VCDOut)$($VRROut)$($VPOut)"
+
+        $VOut += $VInfoOut
+    }
 
     $PCNameOut = "PC Name: $($PCName) `n"
     $OSNameOut = "OS Name: $($OSName) `n"
@@ -29,9 +55,9 @@ Function GetSysInfo {
     $HDTotalOut = "Total Hard Drive Space: $($HDTotal) GB `n"
     $HDFreeOut = "Free Hard Drive Space: $($HDFree) GB `n"
     $NetNameOut = "Network Adapter Used: $($NetName) `n"
-    $NetTypeOut = "Type of Network Adapter Used: $($NetType) `n"
+    $NetTypeOut = "Type of Network Adapter Used: $($NetType) `n`n`n`nVideo Adapters:`n`n"
 
-    $SysInfoOut = "$($PCNameOut)$($OSNameOut)$($OSVerOut)$($CPUNameOut)$($CPUPCoreOut)$($CPUVCoreOut)$($CPUBitOut)$($RAMTotalOut)$($HDTotalOut)$($HDFreeOut)$($NetNameOut)$($NetTypeOut)"
+    $SysInfoOut = "$($PCNameOut)$($OSNameOut)$($OSVerOut)$($CPUNameOut)$($CPUPCoreOut)$($CPUVCoreOut)$($CPUBitOut)$($RAMTotalOut)$($HDTotalOut)$($HDFreeOut)$($NetNameOut)$($NetTypeOut)$($VOut)"
 
     Write-Output $SysInfoOut
 }
